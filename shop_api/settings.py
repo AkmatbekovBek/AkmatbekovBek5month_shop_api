405c5ba1dbc48528e3f9cf59600a4dd47ad8f3c8
AkmatbekovBek <bekakmatbekov54@gmail.com>
2023-11-08 23:47:49 +0600

    hw 5

--- shop_api/settings.py
+++ shop_api/settings.py
@@ -39,7 +39,18 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'product',
+    'rest_framework.authtoken',
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+
+    ]
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
