770b392120b34d35e87f0e01e9749204816c436c
AkmatbekovBek <bekakmatbekov54@gmail.com>
2023-11-19 15:47:38 +0600

    hw 6 and 7

--- shop_api/settings.py
+++ shop_api/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
     'rest_framework',
     'product',
     'rest_framework.authtoken',
+    'users',
 ]
 
 
